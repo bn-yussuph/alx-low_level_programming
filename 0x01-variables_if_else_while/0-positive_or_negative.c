84907ae33f2f3941d014c9c5ee2e276b83ed1689
bn-yussuph <sulehajjaajj@gmail.com>
2024-06-21 16:27:26 -0700

    corrections to task zero

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,7 +12,6 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes here */
-	printf("%d\n", n);
 
 	if(n < 0){
 		printf("%d is negative\n", n);
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+none read me file
