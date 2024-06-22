21b4a46afbf2d63e3ee98acbe32ae0c211f39641
bn-yussuph <sulehajjaajj@gmail.com>
2024-06-21 17:06:27 -0700

    task 2 completed

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes here */
+
+/* betty style doc for function goes here */
+int main(void)
+{
+	char ch = 'a';
+	while(ch <=  'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
