55cc49ee1d9282ca1f8f7e8528f6d933ed23428d
bn-yussuph <sulehajjaajj@gmail.com>
2024-06-22 06:47:16 -0700

    added descriotion for main

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,14 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes here */
-
-/* betty style doc for function goes here */
+/**
+ * main - Entry point
+ *
+ * Description: a function to print a random no.
+ * @parameter: takes no parameter
+ *
+ * Return: Always 0 (Success)
+  */
 int main(void)
 {
 	int n;
