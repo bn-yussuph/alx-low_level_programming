69f1d03e9527a061940d0bef3089cc4e763c4d9e
bn-yussuph <sulehajjaajj@gmail.com>
2024-06-22 06:36:50 -0700

    task 0 refactored to meet betty std

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,13 +13,16 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes here */
 
-	if(n < 0){
+	if (n < 0)
+	{
 		printf("%d is negative\n", n);
 	}
-	else if(n > 0){
+	else if (n > 0)
+	{
 		printf("%d is positive\n", n);
 	}
-	else{
+	else
+	{
 		printf("%d is zero\n", n);
 	}
 
