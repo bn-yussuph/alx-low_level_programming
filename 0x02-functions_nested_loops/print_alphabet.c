27e995b2b59037863bf32841cbcac3ef655fc7b9
bn-yussuph <sulehajjaajj@gmail.com>
2024-06-26 07:48:51 -0700

    Task 1 and 2 amended

--- 0x02-functions_nested_loops/print_alphabet.c
+++ 0x02-functions_nested_loops/print_alphabet.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * main - Entry point
+ *
+ * Description: A fxn to print alphabets
+ *
+ * Return: Always 0 (Succes)
+ */
+
 void print_alphabet(void)
 {
 	char ch = 'a';
