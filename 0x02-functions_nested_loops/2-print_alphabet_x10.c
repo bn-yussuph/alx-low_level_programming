06ad4bb1c8aa8c2ddf48d65cc04c45ed20109f4b
bn-yussuph <sulehajjaajj@gmail.com>
2024-06-25 21:37:47 -0700

    last typo

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+
 /**
  * main - Entry point
  *
@@ -14,8 +15,8 @@ void print_alphabet_x10(void)
 
 	while (count++ < 9)
 	{
-		for (ch = 'a'; ch < 'z'; ch++)
+		for (ch = 'a'; ch <= 'z'; ch++)
 			_putchar(ch);
 		_putchar('\n');
 	}
-}
+i}
