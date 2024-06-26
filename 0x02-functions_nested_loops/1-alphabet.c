874641a6b73688f0647b1fc74bce24bf59da79db
bn-yussuph <sulehajjaajj@gmail.com>
2024-06-25 21:01:33 -0700

    betty made happy

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,17 @@
 #include "main.h"
 
+/**
+ * main - Entry point
+ *
+ * Description: fxn to print alhabets
+ *
+ * Return: void
+ */
 void print_alphabet(void)
 {
 	char ch = 'a';
 
-	while( ch <= 'z')
+	while (ch <= 'z')
 	{
 		_putchar(ch);
 		ch++;
