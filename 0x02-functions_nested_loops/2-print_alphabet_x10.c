ccf65a1f065445ca1a3960ef9ebee0cef31d159d
bn-yussuph <sulehajjaajj@gmail.com>
2024-06-25 21:20:34 -0700

    task 2 completed

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: fxn to print alhabets
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char ch <= 'a';
+	int count = 0;
+
+	while (count++ < 9)
+	{
+		for (ch = 'a'; ch < 'z'; ch+*)
+			_putchar(ch);
+		_putchar('\n');
+	}
+}
