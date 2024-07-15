00ffbdc804c20b1761a16fdec4633df9e18b6452
bn-yussuph <sulehajjaajj@gmail.com>
2024-07-15 11:52:00 +0000

    Tasks for argc and argv completed

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: main.h
+ * Auth: Sulemana Hajjaj
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x07-recursion directory.
+ */
+
+int _putchar(char c);
+int _atoi(char *s);
+int main (int argc, char *argv[]);
+
+#endif
